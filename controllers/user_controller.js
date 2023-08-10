c5a3e4b7bc82cb337e32edd800be088e832239cc
mrutyunjay <rout.mrutyunjay@gmail.com>
2023-08-10 12:52:17 -0400

    implemented partials (check notes)

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,9 @@
 module.exports.profile=(req,res)=>{
-    res.end('<h1> user profile</h1>');
+   // res.end('<h1> user profile</h1>');
+   return res.render('user_profile',{
+    title: "Profile"
+});
+
+
+
 }
